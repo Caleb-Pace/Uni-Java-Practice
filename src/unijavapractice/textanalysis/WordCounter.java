48ffd13d36addf6f8ab0dffe30f3089bdd123922
Caleb Pace <pacecaleb2004+Git@gmail.com>
2024-05-13 21:10:29 +1200

    Added Word Counter class

---
+++ src/unijavapractice/textanalysis/WordCounter.java
@@ -0,0 +1,66 @@
+package unijavapractice.textanalysis;
+
+public class WordCounter {
+    private String text;
+    private int[] frequencies;
+
+    public WordCounter(String text) {
+        setText(text);
+    }
+
+
+    //-/ Set
+    public void setText(String text) {
+        this.text = text;
+        frequencies = keywordFreqency();
+    }
+
+    //-/ Methods
+    public String toString() {
+        String output = "";
+
+        // Format and append keyword-frequency pairs
+        int i = 0; // Links keyword to frequency array
+        for (Keyword keyword : Keyword.values()) {
+            output += String.format("%s: %d ", keyword.getStr(), frequencies[i++]);
+        }
+
+        return output;
+    }
+    
+    // Counts the frequency of each keyword in the text
+    private int[] keywordFreqency() {
+        int[] frequencies = new int[Keyword.values().length]; // Stores keyword frequencies
+        
+        // Loop through each word
+        for (String word : text.split(" ")) {
+            int i = 0; // Reset keyword frequency pointer
+
+            // Loop through each Keyword
+            for (Keyword keyword : Keyword.values()) {
+                // Check if the current word matches the keyword
+                if (keyword.getStr().equals(word))
+                    frequencies[i]++; // Count word
+                
+                i++; // Next keyword
+            }
+        }
+
+        return frequencies;
+    }
+
+    //--/ Data structures
+    public enum Keyword {
+        FOX("fox"), QUICK("quick"), LAZY("lazy"), CAT("cat"); 
+
+        private String word;
+
+        private Keyword(String word) {
+            this.word = word;
+        }
+
+
+        //-/ Get
+        public String getStr() { return word; }
+    }
+}
